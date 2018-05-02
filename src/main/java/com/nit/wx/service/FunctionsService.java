fdcb9c11fea9fe6f5cd86e763b7bda92dea2eddc
littlehen <1036383548@qq.com>
2018-05-02 11:55:54 +0800

    修改

--- src/main/java/com/nit/wx/service/FunctionsService.java
+++ src/main/java/com/nit/wx/service/FunctionsService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.swing.plaf.basic.BasicScrollPaneUI;
+
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -31,10 +32,11 @@ public class FunctionsService {
 
 
     public Map<String,Object> pinglunGongneng(String openId,String plzs,String bcfPl,String yp,String jx,String hd,String sy ,String yz){
-
+    	System.out.println(openId+"123");
         Map<String,Object> map = new HashMap<>();
         UserList user = new UserList();
         user = userDao.findByOpenid(openId);
+        System.out.println(user.getUserid());
         Functions functions = new Functions();
         Date date = new Date();
         Pay pay = new Pay();
